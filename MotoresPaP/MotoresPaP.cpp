86b2f4daab39e2749f85adb2d5a1631384986b7a
Laumaro08 <Laumaro200@gmail.com>
2025-05-07 16:43:34 -0300

    MotoresPaP

--- MotoresPaP/MotoresPaP.cpp
+++ MotoresPaP/MotoresPaP.cpp
@@ -6,6 +6,20 @@ int pasoNormalArray[4][4]={
                   {0,1,1,0},
                   {0,0,1,1},
                   {1,0,0,1}
+int pasoCompletoArray[4][4]={
+                  {1,0,0,0},
+                  {0,1,0,0},
+                  {0,0,1,0},
+                  {0,0,0,1}
+int pasoMedioArray[8][4]={
+                  {1,0,0,0},
+                  {1,1,0,0},
+                  {0,1,0,0},
+                  {0,1,1,0}
+                  {0,0,1,1}
+                  {0,0,1,1}
+                  {0,0,0,1}
+                  {1,0,0,1}
 };
 void pasoNormal_init(){
     int pines[4]={IN0,IN1,IN2,IN3}
@@ -18,12 +32,13 @@ void pasoNormal_init(){
                       &~(1<<i*4+2)&~(1<<i*4+3);
         }
         else{
-            GPIOA->CRH&=~(1<<i*4)&~(1<<i*4+1)
-                      &~(1<<i*4+2)&~(1<<i*4+3);
-            GPIOA->CRH|=(1<<i*4)&~(1<<i*4+1)
-                      &~(1<<i*4+2)&~(1<<i*4+3);
+            GPIOA->CRH&=~(1<<(i%8)*4)&~(1<<(i%8)*4+1)
+                      &~(1<<(i%8)*4+2)&~(1<<(i%8)*4+3);
+            GPIOA->CRH|=(1<<(i%8)*4)&~(1<<(i%8)*4+1)
+                      &~(1<<(i%8)*4+2)&~(1<<(i%8)*4+3);
         }
     }
+}
 void pasoCompleto_init(){
     int pines[4]={IN0,IN1,IN2,IN3}
     for (int i=0;i>4;i++)
@@ -35,12 +50,13 @@ void pasoCompleto_init(){
                       &~(1<<i*4+2)&~(1<<i*4+3);
         }
         else{
-            GPIOA->CRH&=~(1<<i*4)&~(1<<i*4+1)
-                      &~(1<<i*4+2)&~(1<<i*4+3);
-            GPIOA->CRH|=(1<<i*4)&~(1<<i*4+1)
-                      &~(1<<i*4+2)&~(1<<i*4+3);
+            GPIOA->CRH&=~(1<<(i%8)*4)&~(1<<(i%8)*4+1)
+                      &~(1<<(i%8)*4+2)&~(1<<(i%8)*4+3);
+            GPIOA->CRH|=(1<<(i%8)*4)&~(1<<(i%8)*4+1)
+                      &~(1<<(i%8)*4+2)&~(1<<(i%8)*4+3);
         }
     }
+}
 void pasoMedio_init(){
     int pines[4]={IN0,IN1,IN2,IN3}
     for (int i=0;i>4;i++)
@@ -52,14 +68,56 @@ void pasoMedio_init(){
                       &~(1<<i*4+2)&~(1<<i*4+3);
         }
         else{
-            GPIOA->CRH&=~(1<<i*4)&~(1<<i*4+1)
-                      &~(1<<i*4+2)&~(1<<i*4+3);
-            GPIOA->CRH|=(1<<i*4)&~(1<<i*4+1)
-                      &~(1<<i*4+2)&~(1<<i*4+3);
+            GPIOA->CRH&=~(1<<(i%8)*4)&~(1<<(i%8)*4+1)
+                      &~(1<<(i%8)*4+2)&~(1<<(i%8)*4+3);
+            GPIOA->CRH|=(1<<(i%8)*4)&~(1<<(i%8)*4+1)
+                      &~(1<<(i%8)*4+2)&~(1<<(i%8)*4+3);
         }
     }
 }
-void pasoNormal(){
-
+void pasoNormal()
+{
+    int pines[4]={IN0,IN1,IN2,IN3};
+    for(g=pasos;g=0;g--){
+        for(int j=0;j<4;j++){
+            if(pasoNormalArray[g%4][j]){
+                GPIOA->BSRR|=(1<<pines[j]);
+            }
+            else{
+                GPIOA->BSRR|=(1<<pines[j]+16);
+            }
+        }
+    }
+}
+void pasoCompleto(){
+    int pines[4]={IN0,IN1,IN2,IN3};
+    for(int g=pasos;g=0;g--){
+        for(int j=0;j<4;j++){
+            if(pasoCompletoArray[g%4][j]){
+                GPIOA->BSRR|=(1<<pines[j]);
+            }
+            else{
+                GPIOA->BSRR|=(1<<pines[j]+16);
+            }
+        }
+    }
+}
+void pasoMedio()
+{
+    int pines[4]={IN0,IN1,IN2,IN3};
+    for(int g=pasos;g=0;g--){
+        for(int j=0;j<4;j++){
+            if(pasoMedioArray[g%8][j]){
+                GPIOA->BSRR|=(1<<pines[j]);
+            }
+            else{
+                GPIOA->BSRR|=(1<<pines[j]+16);
+            }
+        }
+    }
+}
+void TipoPaso(){
+Function(char tipo){
+    tipo=tolower(tipo);
     }
 }
---
+++ MotoresPaP/Tipos.h
@@ -0,0 +1,5 @@
+#ifndef Tipos_H
+#define Tipos_H
+#include "Arduino.h"
+MotorPaP_init(int IN0, int IN1, int IN2, int IN3);
+MotorPaP(int IN0, int IN1, int IN2, int IN3, int tipoPaso, int pasos);
